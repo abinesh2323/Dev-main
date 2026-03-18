21cc528b47dfc214b49772db2975453719703c9b
abinesh2323 <abineshkalyan2002@gmail.com>
2026-03-18 12:25:07 +0530

    hhhome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import Loading from '@/components/Loading';
-
+import HomeClient from '@/components/HomeClient';
 const Home = dynamic(() => import('@/pages/Home'), {
 	loading: () => <Loading />,
 	ssr: false,
